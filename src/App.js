bbd4b959a4d3648176064edba40dd457e8a63806
kanokenji <aleksandrtarasevich77@gmail.com>
2020-06-14 09:56:40 -0600

    Fix scrolling for mobile phone

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ function App() {
     // https://ru.reactjs.org/docs/hooks-reference.html#useeffect
     useEffect(() => {
         ['scroll', 'touchmove'].forEach(function(e) {
-            window.addEventListener(e, handleScroll);
-            return () => window.removeEventListener(e, handleScroll);
+            document.addEventListener(e, handleScroll);
+            return () => document.removeEventListener(e, handleScroll);
         });
     }, []);
 
