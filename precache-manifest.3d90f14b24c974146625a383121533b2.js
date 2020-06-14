33b7e0880c99f5a8aa6d86b2587bf7e88e018017
kanokenji <aleksandrtarasevich77@gmail.com>
2020-06-14 08:39:11 -0600

    Updates

---
+++ precache-manifest.3d90f14b24c974146625a383121533b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af2350453328ed0a533dbbf6d609f0d2",
+    "url": "/infinity-loader-demo/index.html"
+  },
+  {
+    "revision": "77014a99ea3e3e9fbc8b",
+    "url": "/infinity-loader-demo/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b3a41c1a06f44d2c98aa",
+    "url": "/infinity-loader-demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "77014a99ea3e3e9fbc8b",
+    "url": "/infinity-loader-demo/static/js/2.f9b02be0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/infinity-loader-demo/static/js/2.f9b02be0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3a41c1a06f44d2c98aa",
+    "url": "/infinity-loader-demo/static/js/main.8e5eb2c8.chunk.js"
+  },
+  {
+    "revision": "044966b7be4ffd829cee",
+    "url": "/infinity-loader-demo/static/js/runtime-main.4c2f4e6e.js"
+  }
+]);
