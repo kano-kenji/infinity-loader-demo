5c3d6bb03218f64af862d01fde3bc475523bfc68
kanokenji <aleksandrtarasevich77@gmail.com>
2020-06-14 09:50:04 -0600

    Updates

--- precache-manifest.99b373e43ab9b8f82e7c83d77ef33c84.js
+++ precache-manifest.99b373e43ab9b8f82e7c83d77ef33c84.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fa38bfc4fe8b8ec86b31ca30438ca4cb",
+    "revision": "76410d2c2b4c8c63c88eba70f27001e0",
     "url": "/infinity-loader-demo/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/infinity-loader-demo/static/css/2.af3c1da9.chunk.css"
   },
   {
-    "revision": "4cd945bdc08f7258fc78",
+    "revision": "c30dd06b15abc6d32d30",
     "url": "/infinity-loader-demo/static/css/main.5f361e03.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/infinity-loader-demo/static/js/2.f9b02be0.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4cd945bdc08f7258fc78",
-    "url": "/infinity-loader-demo/static/js/main.ffdeb347.chunk.js"
+    "revision": "c30dd06b15abc6d32d30",
+    "url": "/infinity-loader-demo/static/js/main.f7113877.chunk.js"
   },
   {
     "revision": "044966b7be4ffd829cee",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/infinity-loader-demo/precache-manifest.c55e22cc8bfc5a728bb9aeb1198d981c.js"
+  "/infinity-loader-demo/precache-manifest.99b373e43ab9b8f82e7c83d77ef33c84.js"
 );
 
 self.addEventListener('message', (event) => {
